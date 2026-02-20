c54089471dfb30975fedf2666aeca7cee6dc0462
Kelsie Hughes <khughes4@guilford.edu>
2026-02-20 14:15:26 -0500

    commit from 2-20 class

---
+++ src/main/java/kelsie/h/Individual.java
@@ -0,0 +1,10 @@
+package kelsie.h;
+
+public class Individual {
+// This is the class definition file for the Individual class 
+
+// Attributes 
+private char gender;
+private String job; 
+private int yearsOfwork; 
+}
---
+++ src/main/java/kelsie/h/ProgramEntryPoint.java
@@ -0,0 +1,9 @@
+package kelsie.h;
+
+public class ProgramEntryPoint {
+    // This particular program is a driver program that will run and test other parts of the project
+    public static void main(String[] args) {
+        // we can now instantiate an individual object 
+        Individual theIndividual = new Individual(); 
+    }
+}
